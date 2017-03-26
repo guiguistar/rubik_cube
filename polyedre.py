c29f8c51cd282e374c9072b7d99fb51909901c68
FilipeVasconcelos <filipe.manuel.vasconcelos@gmail.com>
2017-03-26 20:59:41 +0200

    polyedre.py affiche différents types de polygones de l'attribut faces

--- polyedre.py
+++ polyedre.py
@@ -26,6 +26,7 @@ class Polyedre:
     # =============================================================================
     def __init__(self, 
                  sommets, 
+                 aretes,
                  faces,
                  couleurs_faces, # Liste des couleurs des faces
                  vecteur_position = [0, 0, 0],
@@ -34,11 +35,11 @@ class Polyedre:
        
         # Attention à cloner la liste (qui est passée par référence)
         self.sommets = list(sommets) 
+        self.aretes  = aretes
         self.faces = faces
         
         self.couleurs_faces = couleurs_faces
 
-        print vecteur_position
         # Attention ici: la rotation initiale est effectuée avant la translation initiale
         self.rotation_polyedre(angle, vecteur_rotation)
         self.translation_polyedre(vecteur_position)
@@ -58,17 +59,23 @@ class Polyedre:
     def afficher_aretes(self):
         glBegin(GL_LINES)
         for couleur, arete in enumerate(self.aretes):
-            glColor3fv(couleurs_rubik[3])
+            glColor3fv(couleurs_rubik[0])
             for sommet in arete:
                 glVertex3fv(self.sommets[sommet])
         glEnd()
     # =============================================================================
     def afficher_faces(self):
-        glBegin(GL_QUADS)
+        # fmv : necessaire de savoir quel types de polygones on affiche 
+        # fmv : il suffit de déterminer la taille self.faces
+        if len(self.faces[0]) == 4 :
+            glBegin(GL_QUADS)
+        if len(self.faces[0]) == 3 :
+            glBegin(GL_TRIANGLES)
         for couleur, face in enumerate(self.faces):
-            #print couleur,face,self.couleurs_faces
+            #print couleur,face,self.couleurs_faces[couleur]
             glColor3fv(couleurs_rubik[self.couleurs_faces[couleur]])
             for sommet in face:
+             #   print sommet , self.sommets[sommet]
                 glVertex3fv(self.sommets[sommet])
         glEnd()
     # =============================================================================
@@ -80,6 +87,7 @@ class Polyedre:
         self.sommets[:] = [rotation(sommet, angle, vecteur) for sommet in self.sommets]
     # =============================================================================
     def translation_polyedre(self, vecteur):
+        #print "in translation_polyedre",vecteur
         self.sommets[:] = [translation(sommet, vecteur) for sommet in self.sommets]
 
 if __name__=='__main__':
@@ -91,13 +99,78 @@ if __name__=='__main__':
 
     os.environ['SDL_VIDEO_WINDOW_POS'] = "%d,%d" % (infopygame.current_w,infopygame.current_h)
     pygame.display.set_mode(display, DOUBLEBUF|OPENGL)
-    
-    #fmv : creer un test avec un simple polyedre.
+
+    # vitesse de rotation de la caméra
+    pas_rotation_camera = 5 
+
+   
+    sommets = [
+    [-1, +1, +1], 
+    [+1, +1, +1], 
+    [+1, -1, +1], 
+    [-1, -1, +1], 
+    [-1, -1, -1], 
+    [+1, -1, -1], 
+    [+1, +1, -1], 
+    [-1, +1, -1]  ]
+
+    aretes = [
+    [0,1], #AB
+    [1,2], #BC
+    [2,3], #CD 
+    [3,0], #DA
+    [4,5], #EF 
+    [5,6], #FG
+    [6,7], #GH
+    [7,4], #HE
+    [0,7], #AH 
+    [1,6], #BG
+    [2,5], #CF
+    [3,4]  #DE
+    ]
+
+    faces = [
+    [0,1,2,3], #ABCD  avant
+    [4,5,6,7], #EFGH  arriere
+    [0,1,6,7], #ABGH  haut
+    [4,3,2,5], #EDCF  bas
+    [0,7,4,3], #AHED  gauche
+    [1,6,5,2]  #BGFC  droite
+    ]
+
+    couleurs_faces=[4,4,4,4,4,4]
+    # classe gestion clavier global qq soit le polyedre
+    gestion_clavier=Gestion_clavier()
+
+    #instanciation de la classe Polyedre
+    test = Polyedre(sommets, aretes,faces, couleurs_faces)
+    # pyOpenGl
+    gluPerspective(45, display[0]/display[1], 0.1, 50.0)
+    glTranslatef(0.0,0.0, -8) 
+    glEnable(GL_DEPTH_TEST) # Permet de cacher les objets placés derrière d'autres objets
 
     # ===================
     #   main event loop
     # ===================
-    #while True:
+    while True:
+        # Gestion des événements clavier
+        for event in pygame.event.get(): gestion_clavier.check_event_key(event)
+
+          # Mouvements de la caméra
+        if gestion_clavier.fleche_gauche: glRotatef(pas_rotation_camera, 0, 1, 0)
+        if gestion_clavier.fleche_droite: glRotatef(-pas_rotation_camera, 0, 1, 0)
+        if gestion_clavier.fleche_haut:   glRotatef(pas_rotation_camera, 1, 0, 0)
+        if gestion_clavier.fleche_bas:    glRotatef(-pas_rotation_camera, 1, 0, 0)
+        if gestion_clavier.touche_p:      glRotatef(pas_rotation_camera, 0, 0, 1)
+        if gestion_clavier.touche_m:      glRotatef(-pas_rotation_camera, 0, 0, 1)
+ 
+
+        # Mise à jour de l'affichage
+        glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT)
+
+        test.afficher()
+        test.afficher_aretes()
+        pygame.display.flip()
 
 
 
